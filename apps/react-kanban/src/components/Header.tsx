3db113e399fe800ceb2ad187208d5d6ec4b85387
Dmitrii Goncharov <homeboundx@gmail.com>
2025-09-06 18:04:32 +0300

    Base

---
+++ apps/react-kanban/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  display: flex,
+  height: 60px,
+`;
+
+export const Header = () => {
+  return <StyledHeader>some header</StyledHeader>;
+};
---
+++ apps/react-kanban/src/components/Sidebar.tsx
@@ -0,0 +1,14 @@
+export const Sidebar = () => {
+  return (
+    <aside>
+      <div>
+        <div>Logo</div>
+        <div>All boards</div>
+      </div>
+      <div>
+        <div>theme switcher</div>
+        <div> hide sidebar toggle</div>
+      </div>
+    </aside>
+  );
+};
